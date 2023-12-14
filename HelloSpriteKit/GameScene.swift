9c8267e7534a72b283fbfadc86d58ef3b74535ad
coding-campus-ai <153612007+coding-campus-ai@users.noreply.github.com>
2023-12-14 09:28:03 +0900

    Update GameScene.swift

--- HelloSpriteKit/GameScene.swift
+++ HelloSpriteKit/GameScene.swift
@@ -8,10 +8,10 @@
 import SpriteKit
 
 class GameScene:SKScene {
-    let label = SKLabelNode(text: "Hello SpriteKit!")
+    let label = SKLabelNode(text: "Hello Coding Campus!")
 
     override func didMove(to view: SKView) {
-        label.fontSize = 45
+        label.fontSize = 30
         label.fontColor = SKColor.magenta
         label.fontName = "GillSans-Bold"
         label.position = CGPoint(x:view.frame.width / 2, y:view.frame.height / 2)
